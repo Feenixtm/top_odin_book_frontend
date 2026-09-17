52b97daca957a64ba54f44aa9ac38d90623a5ee3
feenixtm <177155245+Feenixtm@users.noreply.github.com>
2026-09-16 17:28:19 -0700

    Installed React with Vite and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='text-[4rem]'>Well hello there</p>
+    </>
+  )
+}
+
+export default App
