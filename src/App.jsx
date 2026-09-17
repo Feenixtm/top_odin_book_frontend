680ca8bed9b5da53931128b7a446b77840b60fc3
feenixtm <177155245+Feenixtm@users.noreply.github.com>
2026-09-17 13:04:44 -0700

    Added height and bg-color to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
+
 import Home from './components/Home';
 import DevHeader from './components/DevHeader';
 import SignUp from './components/forms/SignUp';
@@ -13,7 +14,7 @@ function App() {
       
       <DevHeader/>
 
-      <main className='p-4 md:px-8'>
+      <main className='h-[calc(100vh-3.5rem-1px)] p-4 md:px-8 bg-emerald-200'>
 
         <Routes>
           <Route path="/" element={ <Home/> }></Route>
